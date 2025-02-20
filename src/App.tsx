3d9069c1190f942210913595e8351c571d48cc5b
Roman <62247180+Roman-Shubnikov@users.noreply.github.com>
2025-02-20 21:59:14 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-function a() {
-
+type UserResType = {
+  id: number,
+  email: string,
 }
 
-
+const formatUser = (user: UserResType) => {
+  return (
+    <div key={user.id}>
+      <div>{user.email}</div>
+      <div>{user.id}</div>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [res, setRes] = useState<UserResType[] | null>(null);
+
+  const buttonClick = () => {
+    setCount((c) => c + 1)
+    setCount((c) => c + 1)
+    setCount((c) => c + 1)
+  }
+
+  const getUsers = async () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.json())
+    .catch(() => new Promise((resolve) => {resolve({ a: 1 })}))
+    .then((data) => setRes(data))
+    .finally(() => {})
+    
+  }
+
+  
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+  
 
   return (
     <>
@@ -24,14 +54,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      {res ? res.map((v) => formatUser(v)) : null}
+      <div className="card"></div>
+        <button onClick={buttonClick}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
